d11e4e0d0119ad4e5a990e149660abd4ef47b638
madahetooo <ieslammedhat@gmail.com>
2022-04-27 23:44:53 +0200

    adding firebase authentication and firestore functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,158 @@
-import 'package:flutter/gestures.dart';
+// import 'package:flutter/gestures.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutterwidgets/signUp.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'home.dart';
+//
+// void main() async{
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+//   runApp(MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Hello World Flutter Application',
+//       theme: ThemeData(
+//         // This is the theme of your application.
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: home(),
+//     );
+//   }
+// }
+//
+// class MyHomePage extends StatelessWidget {
+//   // MyHomePage({Key key, this.title}) : super(key: key);
+//
+//   // This widget is the home page of your application.
+//   // final String title;
+//   final TextEditingController emailController = new TextEditingController();
+//   final TextEditingController passwordController = new TextEditingController();
+//   final _auth = FirebaseAuth.instance;
+//
+//   // string for displaying the error Message
+//   String? errorMessage;
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: AppBar(
+//           title: Text(
+//             "eslam",
+//             style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+//           ),
+//           leading: Icon(Icons.icecream),
+//           actions: [Icon(Icons.star)],
+//         ),
+//         body: Padding(
+//           padding: EdgeInsets.all(20.0),
+//           child: Column(
+//             children: <Widget>[
+//               Container(
+//                 width: 200,
+//                 height: 200,
+//                 child: Image.network(
+//                     'https://ibwc.in/wp-content/uploads/2021/06/log.png'),
+//               ),
+//              TextFormField(
+//                   controller: emailController,
+//
+//                   keyboardType: TextInputType.emailAddress,
+//                   onSaved: (value) {
+//                     emailController.text = value!;
+//                   },
+//                   decoration: InputDecoration(
+//                     labelText: "Email Address",
+//                     border: OutlineInputBorder(),
+//                   ),
+//
+//                 ),
+//               Padding(
+//                 padding: EdgeInsets.all(20.0),
+//                 child: TextField(
+//                   obscureText: true,
+//                   decoration: InputDecoration(
+//                     labelText: "Password",
+//                     border: OutlineInputBorder(),
+//                   ),
+//                 ),
+//               ),
+//               Padding(
+//                 padding: EdgeInsets.all(10),
+//                 child: Container(
+//                   decoration: BoxDecoration(
+//                       gradient: LinearGradient(
+//                           begin: Alignment.bottomCenter,
+//                           end: Alignment.topCenter,
+//                           colors: [Colors.red, Colors.blueAccent])),
+//                   child: FlatButton(
+//                     child: Text(
+//                       "Login",
+//                       style: TextStyle(
+//                           color: Colors.white, fontWeight: FontWeight.bold),
+//                     ),
+//                     onPressed: () {
+//                       Navigator.push(context, MaterialPageRoute(
+//                         builder: (context) => home(),
+//                       ));
+//                     },
+//                   ),
+//                 ),
+//               ),
+//               Padding(
+//                 padding: EdgeInsets.all(10.0),
+//                 child: RichText(
+//                   text: TextSpan(
+//                       text: 'Dont have an account?!',
+//                       style: TextStyle(color: Colors.black, fontSize: 15),
+//                       children: <TextSpan>[
+//                         TextSpan(
+//                             text: 'Sign Up',
+//                             style: TextStyle(color: Colors.red, fontSize: 15),
+//                             recognizer: TapGestureRecognizer()..onTap = () {
+//                               Navigator.push(context, MaterialPageRoute(
+//                                 builder: (context) => MyForm(),
+//                               ));
+//                             })
+//                       ]),
+//                 ),
+//               )
+//             ],
+//           ),
+//         ));
+//   }
+// }
+
+
+
+
+import 'package:flutterwidgets/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutterwidgets/signUp.dart';
 
-import 'home.dart';
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 
-void main() => runApp(MyApp());
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hello World Flutter Application',
+      title: 'Email And Password Login',
       theme: ThemeData(
-        // This is the theme of your application.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
-      home: MyHomePage(title: 'Login'),
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
     );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application.
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            this.title,
-            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
-          ),
-          leading: Icon(Icons.icecream),
-          actions: [Icon(Icons.star)],
-        ),
-        body: Padding(
-          padding: EdgeInsets.all(20.0),
-          child: Column(
-            children: <Widget>[
-              Container(
-                width: 200,
-                height: 200,
-                child: Image.network(
-                    'https://ibwc.in/wp-content/uploads/2021/06/log.png'),
-              ),
-              Padding(
-                padding: EdgeInsets.all(20.0),
-                child: TextField(
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    labelText: "Email Address",
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(20.0),
-                child: TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: "Password",
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(10),
-                child: Container(
-                  decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                          begin: Alignment.bottomCenter,
-                          end: Alignment.topCenter,
-                          colors: [Colors.red, Colors.blueAccent])),
-                  child: FlatButton(
-                    child: Text(
-                      "Login",
-                      style: TextStyle(
-                          color: Colors.white, fontWeight: FontWeight.bold),
-                    ),
-                    onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(
-                        builder: (context) => home(),
-                      ));
-                    },
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(10.0),
-                child: RichText(
-                  text: TextSpan(
-                      text: 'Dont have an account?!',
-                      style: TextStyle(color: Colors.black, fontSize: 15),
-                      children: <TextSpan>[
-                        TextSpan(
-                            text: 'Sign Up',
-                            style: TextStyle(color: Colors.red, fontSize: 15),
-                            recognizer: TapGestureRecognizer()..onTap = () {
-                              Navigator.push(context, MaterialPageRoute(
-                                builder: (context) => MyForm(),
-                              ));
-                            })
-                      ]),
-                ),
-              )
-            ],
-          ),
-        ));
   }
 }
---
+++ lib/registration_screen.dart
@@ -0,0 +1,295 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutterwidgets/home.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterwidgets/user_model.dart';
+class RegistrationScreen extends StatefulWidget {
+  const RegistrationScreen({Key? key}) : super(key: key);
+
+  @override
+  _RegistrationScreenState createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  final _auth = FirebaseAuth.instance;
+
+  // string for displaying the error Message
+  String? errorMessage;
+
+
+  // our form key
+  final _formKey = GlobalKey<FormState>();
+  // editing Controller
+  final firstNameEditingController = new TextEditingController();
+  final secondNameEditingController = new TextEditingController();
+  final emailEditingController = new TextEditingController();
+  final passwordEditingController = new TextEditingController();
+  final confirmPasswordEditingController = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    //first name field
+    final firstNameField = TextFormField(
+        autofocus: false,
+        controller: firstNameEditingController,
+        keyboardType: TextInputType.name,
+        validator: (value) {
+          RegExp regex = new RegExp(r'^.{3,}$');
+          if (value!.isEmpty) {
+            return ("First Name cannot be Empty");
+          }
+          if (!regex.hasMatch(value)) {
+            return ("Enter Valid name(Min. 3 Character)");
+          }
+          return null;
+        },
+        onSaved: (value) {
+          firstNameEditingController.text = value!;
+        },
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+          prefixIcon: Icon(Icons.account_circle),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          label: Text("First Name"),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ));
+    //second name field
+    final secondNameField = TextFormField(
+        autofocus: false,
+        controller: secondNameEditingController,
+        keyboardType: TextInputType.name,
+        validator: (value) {
+          if (value!.isEmpty) {
+            return ("Second Name cannot be Empty");
+          }
+          return null;
+        },
+        onSaved: (value) {
+          secondNameEditingController.text = value!;
+        },
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+          prefixIcon: Icon(Icons.account_circle),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          label: Text("Second Name"),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ));
+    //email field
+    final emailField = TextFormField(
+        autofocus: false,
+        controller: emailEditingController,
+        keyboardType: TextInputType.emailAddress,
+        validator: (value) {
+          if (value!.isEmpty) {
+            return ("Please Enter Your Email");
+          }
+          // reg expression for email validation
+          if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
+              .hasMatch(value)) {
+            return ("Please Enter a valid email");
+          }
+          return null;
+        },
+        onSaved: (value) {
+          firstNameEditingController.text = value!;
+        },
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+          prefixIcon: Icon(Icons.mail),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Email",
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ));
+    //password field
+    final passwordField = TextFormField(
+        autofocus: false,
+        controller: passwordEditingController,
+        obscureText: true,
+        validator: (value) {
+          RegExp regex = new RegExp(r'^.{6,}$');
+          if (value!.isEmpty) {
+            return ("Password is required for login");
+          }
+          if (!regex.hasMatch(value)) {
+            return ("Enter Valid Password(Min. 6 Character)");
+          }
+        },
+        onSaved: (value) {
+          firstNameEditingController.text = value!;
+        },
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+          prefixIcon: Icon(Icons.vpn_key),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Password",
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ));
+    //confirm password field
+    final confirmPasswordField = TextFormField(
+        autofocus: false,
+        controller: confirmPasswordEditingController,
+        obscureText: true,
+        validator: (value) {
+          if (confirmPasswordEditingController.text !=
+              passwordEditingController.text) {
+            return "Password don't match";
+          }
+          return null;
+        },
+        onSaved: (value) {
+          confirmPasswordEditingController.text = value!;
+        },
+        textInputAction: TextInputAction.done,
+        decoration: InputDecoration(
+          prefixIcon: Icon(Icons.vpn_key),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          label: Text("Confirm Password"),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ));
+    //signup button
+    final signUpButton = Material(
+      elevation: 5,
+      borderRadius: BorderRadius.circular(30),
+      color: Colors.redAccent,
+      child: MaterialButton(
+          padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          minWidth: MediaQuery.of(context).size.width,
+          onPressed: () {
+            signUp(emailEditingController.text, passwordEditingController.text);
+          },
+          child: Text(
+            "SignUp",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),
+          )),
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.red),
+          onPressed: () {
+            // passing this to our root
+            Navigator.of(context).pop();
+          },
+        ),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Container(
+            color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.all(36.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(
+                        height: 180,
+                        child: Image.asset(
+                          "assets/logo.png",
+                          fit: BoxFit.contain,
+                        )),
+                    SizedBox(height: 45),
+                    firstNameField,
+                    SizedBox(height: 20),
+                    secondNameField,
+                    SizedBox(height: 20),
+                    emailField,
+                    SizedBox(height: 20),
+                    passwordField,
+                    SizedBox(height: 20),
+                    confirmPasswordField,
+                    SizedBox(height: 20),
+                    signUpButton,
+                    SizedBox(height: 15),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+  void signUp(String email, String password) async {
+    if (_formKey.currentState!.validate()) {
+      try {
+        await _auth
+            .createUserWithEmailAndPassword(email: email, password: password)
+            .then((value) => {postDetailsToFirestore()})
+            .catchError((e) {
+          // Fluttertoast.showToast(msg: e!.message);
+        });
+      } on FirebaseAuthException catch (error) {
+        switch (error.code) {
+          case "invalid-email":
+            errorMessage = "Your email address appears to be malformed.";
+            break;
+          case "wrong-password":
+            errorMessage = "Your password is wrong.";
+            break;
+          case "user-not-found":
+            errorMessage = "User with this email doesn't exist.";
+            break;
+          case "user-disabled":
+            errorMessage = "User with this email has been disabled.";
+            break;
+          case "too-many-requests":
+            errorMessage = "Too many requests";
+            break;
+          case "operation-not-allowed":
+            errorMessage = "Signing in with Email and Password is not enabled.";
+            break;
+          default:
+            errorMessage = "An undefined Error happened.";
+        }
+        // Fluttertoast.showToast(msg: errorMessage!);
+        print(error.code);
+      }
+    }
+  }
+  postDetailsToFirestore() async {
+    // calling our firestore
+    // calling our user model
+    // sedning these values
+
+    FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+    User? user = _auth.currentUser;
+
+    UserModel userModel = UserModel();
+
+    // writing all the values
+    userModel.email = user!.email;
+    userModel.uid = user.uid;
+    userModel.firstName = firstNameEditingController.text;
+    userModel.secondName = secondNameEditingController.text;
+
+    await firebaseFirestore
+        .collection("users")
+        .doc(user.uid)
+        .set(userModel.toMap());
+    // Fluttertoast.showToast(msg: "Account created successfully :) ");
+
+    Navigator.pushAndRemoveUntil(
+        (context),
+        MaterialPageRoute(builder: (context) => home()),
+            (route) => false);
+  }
+}
